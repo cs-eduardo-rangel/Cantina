bcad0fb8d6322b05fcc67c5ce1945f9e6815133b
Eduardo Rangel <eduardo.rangel@concretesolutions.com.br>
2015-10-25 17:00:40 -0200

    Implementar Login com o Google
    - Criando Bridging-Header.

---
+++ Cantina-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Cantina-Bridging-Header.h
+//  Cantina
+//
+//  Created by Eduardo Rangel on 10/25/15.
+//  Copyright © 2015 Concrete Solutions. All rights reserved.
+//
+
+#ifndef Cantina_Bridging_Header_h
+#define Cantina_Bridging_Header_h
+
+
+#endif /* Cantina_Bridging_Header_h */
